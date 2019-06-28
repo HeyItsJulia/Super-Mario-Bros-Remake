85a9c5b7396646c55ed9f7f38c6f9e113fbe571b
Lawrence Lundblad <lawrence.lundblad@digipen.edu>
2019-06-28 14:21:02 -0700

    Worked on Death by mario

---
+++ Super Mario Bros/Assets/DeathbyMario.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathbyMario : MonoBehaviour
+{
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.GetComponent<PlayerController>())
+        {
+            Rigidbody otherRB = collision.transform.GetComponent<Rigidbody>();
+            Rigidbody myRB = GetComponent<Rigidbody>();
+            if (otherRB && myRB && otherRB.velocity.y < myRB.velocity.y)
+            {
+                Die();
+            }
+        }
+    }
+}
