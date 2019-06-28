c5878aee904c53e52f780cda1f65276ee38c4d82
Lawrence Lundblad <lawrence.lundblad@digipen.edu>
2019-06-28 11:10:00 -0700

    Super Mario Brothers Progress

---
+++ Super Mario Bros/Assets/EnemyTurnaround.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTurnaround : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.GetComponent<EnemyAI>())
+        {
+            other.GetComponent<EnemyAI>().speed *= 1;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawCube(transform.position, Vector3.one);
+    }
+}
