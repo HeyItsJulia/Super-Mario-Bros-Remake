d8844bf6b546471d06c41011685d9d97a5fb8965
Julia Lundblad <julia.lundblad@digipen.com>
2019-06-28 14:44:40 -0700

    Edited Title Screen Imported New Asset

---
+++ Super Mario Bros/Assets/ChangeScene.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    public void NewScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void Quit()
+    {
+        Debug.Log("I'm Quitting!");
+        Application.Quit();
+    }
+}
