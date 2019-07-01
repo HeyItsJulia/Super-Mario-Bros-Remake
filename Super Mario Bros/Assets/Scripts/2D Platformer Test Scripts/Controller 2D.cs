2379cd43a0a7907bc11ad0e44757199e0fb66e55
Julia Lundblad <julia.lundblad@digipen.com>
2019-07-01 13:57:07 -0700

    stuff

---
+++ Super Mario Bros/Assets/Scripts/2D Platformer Test Scripts/Controller 2D.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (BoxCollider2D))]
+public class Controller2D : MonoBehaviour
+{
+    BoxCollider2D Collider;
+
+    void Start() {
+        Collider = GetComponent<BoxCollider2D>();
+    }
+    
+}
--- Super Mario Bros/Assets/Scripts/ChangeScene.cs
+++ Super Mario Bros/Assets/Scripts/ChangeScene.cs
@@ -13,5 +13,4 @@ public class ChangeScene : MonoBehaviour
         SceneManager.LoadScene(sceneName);
     }
 
-    
 }
