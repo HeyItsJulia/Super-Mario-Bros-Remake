0c5703e8cc21d38e97f6c123dfe02c5cbcfee623
Julia Lundblad <julia.lundblad@digipen.com>
2019-07-02 13:55:41 -0700

    Semi-Finished Timer

--- Super Mario Bros/Assets/Scripts/Timer.cs
+++ Super Mario Bros/Assets/Scripts/Timer.cs
@@ -2,37 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
-public class Timer : MonoBehaviour
+public class Timer : PlayerController
 {
-    public float timeStart = 60;
-    public Text textBox;
-    public Vector3 respawn;
+    public int timeLeft = 5;
+    public Text countdownText;
+   
     // Use this for initialization
     void Start()
     {
-        textBox.text = timeStart.ToString();
+        StartCoroutine("LoseTime");
+    }
 
-    }
-    public void Respawn()
-    {
-        transform.position = respawn;
-        ResetCam();
-    }
-    public void ResetCam()
-    {
-        Camera.main.transform.position = new Vector3(transform.position.x + 11, 0, -10);
-    }
+  
+    
     // Update is called once per frame
     void Update()
     {
-        timeStart -= Time.deltaTime;
-        textBox.text = Mathf.Round(timeStart).ToString();
+        countdownText.text = ("Time Left = " + timeLeft);
 
-        if (timeStart == 0)
+        if (timeLeft <= 0)
         {
+            StopCoroutine("LoseTime");
+            countdownText.text = "Times Up!";
             Respawn();
         }
     }
-    
+
+    IEnumerator LoseTime()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1);
+            timeLeft--;
+        }
+    }
+
 }
