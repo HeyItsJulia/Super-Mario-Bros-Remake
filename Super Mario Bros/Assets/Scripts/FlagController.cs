f28b35d488d284ee661934f61f4d9a052c5fa315
Isaac Richards <isaac.richards@digipen.edu>
2019-07-03 14:04:21 -0700

    flag start 2

--- Super Mario Bros/Assets/Scripts/FlagController.cs
+++ Super Mario Bros/Assets/Scripts/FlagController.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class FlagController : MonoBehaviour
 {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+
+        }
+    }
     // Start is called before the first frame update
     void Start()
     {
