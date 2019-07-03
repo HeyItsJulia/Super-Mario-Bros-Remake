932a4cfa1bf58acf3d8041ff2cace703bfe9754a
Isaac Richards <isaac.richards@digipen.edu>
2019-07-03 13:05:51 -0700

    underground

---
+++ Super Mario Bros/Assets/Scripts/EnvironmentCoinScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnvironmentCoinScript : MonoBehaviour
+{
+    public GameObject Coin;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Collision!");
+        if (other.tag == "Player")
+        {
+            Debug.Log("With Player!");
+            GameObject newCoin = Instantiate(Coin);
+            newCoin.transform.position = transform.position;
+            PlayerController.score += 100;
+            Destroy(gameObject);
+        }
+    }
+}
