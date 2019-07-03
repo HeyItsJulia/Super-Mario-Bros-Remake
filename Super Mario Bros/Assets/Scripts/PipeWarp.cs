95ebf852ac05b3051aae3cf5ec1094131edbc6ab
Isaac Richards <isaac.richards@digipen.edu>
2019-07-03 11:26:30 -0700

    stuff

--- Super Mario Bros/Assets/Scripts/PipeWarp.cs
+++ Super Mario Bros/Assets/Scripts/PipeWarp.cs
@@ -1,20 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 public class PipeWarp : MonoBehaviour
 {
-    Rigidbody rb;
 
-    private void OnTriggerEnter(Collider other)
+    void OnTriggerStay(Collider other)
     {
         if (other.tag == "Player")
         {
-            if (other.attachedRigidbody == true)
+            Debug.Log("Trigger stay!");
+            if (other.GetComponent<PlayerController>() && Input.GetKey(other.GetComponent<PlayerController>().Down)
+                || other.GetComponentInParent<PlayerController>() && Input.GetKey(other.GetComponentInParent<PlayerController>().Down))
             {
-                Debug.Log("true");
+                Debug.Log("Scene Change!");
+                SceneManager.LoadScene("1-1 Underground");
             }
-            
-            
         }
     }
 }
